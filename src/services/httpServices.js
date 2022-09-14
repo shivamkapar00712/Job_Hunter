c6e791989aca524b20814a23e8805f2b0ad378c6
shivam <shivamkapar00712@gmail.com>
2022-09-14 01:37:33 -0700

    uploading

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import {getJWT} from "./authService";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = "";
 function setJWT(jwt){
   axios.defaults.headers.common['authorization'] =jwt;
   axios.defaults.headers.common['Authorization'] =jwt;
